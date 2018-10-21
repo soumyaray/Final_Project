240bdeac9927f42e8c227da26c12e9abf7b3d267
sandy <sandy.wu@iss.nthu.edu.tw>
2018-10-21 20:06:53 +0800

    update Rakefile and refactor from all quality checks

--- Rakefile
+++ Rakefile
@@ -1,20 +1,38 @@
+# frozen_string_literal: false
+
 require 'rake/testtask'
 
-desc 'run tests'
-task :spec do 
-    sh 'ruby spec/unsplash_api_spec.rb'
+task :default do
+  puts `rake -T`
 end
 
-namespace :quality do 
-    task :flog do 
-        sh 'flog lib/'
-    end
+desc 'run tests'
+task :spec do
+  sh 'ruby spec/unsplash_api_spec.rb'
+end
 
-    task :reek do
-        sh 'reek lib/'
+namespace :vcr do
+  desc 'delete cassette fixtures'
+  task :wipe do
+    sh 'rm spec/fixtures/cassettes/*.yml' do |ok, _|
+      puts(ok ? 'Cassettes deleted' : 'No cassettes found')
     end
+  end
+end
 
-    task :rubocop do 
-        sh 'rubocop'
-    end
+namespace :quality do
+  desc 'run all quality checks'
+  task all: %i[rubocop reek flog]
+
+  task :flog do
+    sh 'flog lib/'
+  end
+
+  task :reek do
+    sh 'reek lib/'
+  end
+
+  task :rubocop do
+    sh 'rubocop'
+  end
 end
--- spec/unsplash_api_spec.rb
+++ spec/unsplash_api_spec.rb
@@ -1,26 +1,26 @@
 require_relative 'spec_helper.rb'
 
 describe 'Tests API library' do
-    VCR.configure do |c|
-        c.cassette_library_dir =  CASSETTES_FOLDER
-        c.hook_into :webmock
+  VCR.configure do |c|
+    c.cassette_library_dir =  CASSETTES_FOLDER
+    c.hook_into :webmock
 
-        c.filter_sensitive_data('<UNSPLASH_ACCESS_KEY>') { ACCESS_KEY }
-        c.filter_sensitive_data('<UNSPLASH_SECRET_KEY>') { SECRET_KEY }
-    end
+    c.filter_sensitive_data('<UNSPLASH_ACCESS_KEY>') { ACCESS_KEY }
+    c.filter_sensitive_data('<UNSPLASH_SECRET_KEY>') { SECRET_KEY }
+  end
 
-    before do
-        VCR.insert_cassette CASSETTE_FILE_UNSPLASH, record: :new_episodes ,match_requests_on: %i[method  uri headers]
-    end
+  before do
+    VCR.insert_cassette CASSETTE_FILE_UNSPLASH, record: :new_episodes ,match_requests_on: %i[method  uri headers]
+  end
 
-    after do
-        VCR.eject_cassette
-    end
+  after do
+    VCR.eject_cassette
+  end
 
-    describe 'Picture URL' do 
-        it 'HAPPY : should provide correct link' do 
-            picture = SeoAssistant::Unsplash_API.new(ACCESS_KEY, KEYWORD_E).get_picture
-            _(picture.url).must_equal CORRECT
-        end
+  describe 'Picture URL' do 
+    it 'HAPPY : should provide correct link' do 
+      picture = SeoAssistant::Unsplash_API.new(ACCESS_KEY, KEYWORD_E).get_picture
+      _(picture.url).must_equal CORRECT
     end
+  end
 end
