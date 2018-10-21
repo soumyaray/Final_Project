78bcfb4c8dc265e75066b5c9a71cecf238487650
shykuopo <Sam82267535@yahoo.com.tw>
2018-10-21 19:12:29 +0800

    update unsplash_api and picture

--- lib/picture.rb
+++ lib/picture.rb
@@ -1,13 +1,19 @@
 module SeoAssistant
-    class Picture
-        def initialize(pictures_data)
-            @all_pictures = pictures_data['results']
-            @picture = pictures_data['results'][0]
-        end
+	class Picture
+		def initialize(pictures_data, num = 0)
+			@all_pictures = pictures_data['results']
+			@pictures = pictures_data['results'][0..num-1]
+			@picture = pictures_data['results'][0]
+		end
 
-        def url()
-            urls = @picture['urls']
-            urls['raw']
-        end
-    end
+		def url()
+			urls = @picture['urls']
+			urls['raw']
+		end
+
+		def all_url()
+			all_urls = @pictures.map { |picture| picture['urls'] }
+			all_url = all_urls.map { |url| url['raw'] }
+		end
+	end
 end
--- lib/unsplash_api.rb
+++ lib/unsplash_api.rb
@@ -4,37 +4,34 @@ require 'unsplash'
 require_relative 'picture.rb'
 
 module SeoAssistant
-    class Unsplash_API
+	class Unsplash_API
 
-        def initialize(access_key, keyword_str)
-            #config = YAML.safe_load(File.read('../config/secrets.yml'))
-            #@unsplash_access_key = config["UNSPLASH_ACCESS_KEY"]
-            #@unsplash_secret_key = config["UNSPLASH_SECRET_KEY"]
-            @unsplash_access_key = access_key
-            @query = keyword_str
-        end
+		def initialize(access_key, keyword_str)
+			@unsplash_access_key = access_key
+			@query = keyword_str
+		end
 
-        def unsplash_api_path(query)
-            'https://api.unsplash.com/' + '/search/photos?query=' + query
-        end
+		def unsplash_api_path(query)
+			'https://api.unsplash.com/' + '/search/photos?query=' + query
+		end
 
-        def auth(access_key)
-            "Client-ID " + access_key
-        end
+		def auth(access_key)
+			"Client-ID " + access_key
+		end
 
-        def call_unsplash_url(access_key, url)
-            HTTP.headers(
-                'Authorization' =>  auth(access_key),
-                ).get(url)
-        end
+		def call_unsplash_url(access_key, url)
+			HTTP.headers(
+					'Authorization' =>  auth(access_key),
+					).get(url)
+		end
 
-        def get_picture()
-            url = unsplash_api_path(@query)
-            pictures_data = call_unsplash_url(@unsplash_access_key, url).parse
-            Picture.new(pictures_data)
-            #pictures_data['results'][0]['urls']['raw']
-        end
-    end
+		def get_picture()
+			url = unsplash_api_path(@query)
+			pictures_data = call_unsplash_url(@unsplash_access_key, url).parse
+			Picture.new(pictures_data)
+			#pictures_data['results'][0]['urls']['raw']
+		end
+	end
 end
 
 #config = YAML.safe_load(File.read('../config/secrets.yml'))
